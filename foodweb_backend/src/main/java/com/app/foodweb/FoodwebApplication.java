c8cac8826b51c9ef968e6b57dae756caebfa8a02
SigaukeCogitater.com <cogi@gmail.com>
2020-04-23 05:59:39 +0900

    Added Design Document

---
+++ foodweb_backend/src/main/java/com/app/foodweb/FoodwebApplication.java
@@ -0,0 +1,13 @@
+package com.app.foodweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodwebApplication.class, args);
+	}
+
+}
