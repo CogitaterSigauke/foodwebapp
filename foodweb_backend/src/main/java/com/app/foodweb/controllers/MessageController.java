aa38b273471e9169a141eea1d011405dd0b580ae
SigaukeCogitater.com <cogi@gmail.com>
2020-04-27 01:50:00 +0900

    -Added controllers

---
+++ foodweb_backend/src/main/java/com/app/foodweb/controllers/MessageController.java
@@ -0,0 +1,27 @@
+package com.app.foodweb.controllers;
+
+import com.app.foodweb.models.Message;
+import com.app.foodweb.repositories.MessageRepository;
+
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class MessageController {
+	
+	
+ 
+                
+}
