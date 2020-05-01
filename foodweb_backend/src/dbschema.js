815cc3f6cbcf8f98c93372e462ea26aac30aa37a
SigaukeCogitater.com <cogi@gmail.com>
2020-05-01 21:18:04 +0900

    Edited User Model

---
+++ foodweb_backend/src/dbschema.js
@@ -0,0 +1,91 @@
+let db = {
+    ideas:[
+        {
+            userName: 'user',
+            body: 'Body of the idea',
+            createdAt: '2019-11-24T17:45:38.528Z',
+            likeCount: 5,
+            commentCount: 2
+
+        }
+
+    ],
+    competitions:[
+        {
+            email: "newcompanyacc@gmail.com",
+            acccountType: "company",
+            userName: "The New Company",
+            password: "123456789",
+            confirmPassword: "123456789",
+            companyName: "My Company",
+            companySite: "www.newcompany.com",
+            phoneNumber: "07738982323"
+        }
+
+    ],
+    users:[
+        {
+            createdAt: "2019-11-24T22:22:16.558Z",
+            email: "486tatesdsdbda@gmail.com",
+            userId: "FB0WPpX25ES08N0hAL7NRbuCfyq1",
+            userName: "tatencda" 
+        }
+    ],
+    comments:[
+        {
+            userName: 'user',
+            ideaId: "jaksdjahgkdaksdhakdgsk",
+            body: 'nice idea',
+            createdAt: "2019-11-24T22:22:16.558Z"
+        }        
+    ],
+    notifications: [
+        {
+            recipient: 'user',
+            sender: 'mhamha',
+            read: 'true | false',
+            ideaId: 'jaksjdhakjshdakjshdkauwqueh',
+            type: 'like | comment | register',
+            createdAt: "2019-11-24T22:22:16.558Z"
+        }
+
+
+    ]
+
+}
+
+const userDetails = {
+    credentials: {
+        email: "newcompanyacc@gmail.com",
+        acccountType: "company",
+        userName: "The New Company",
+        password: "123456789",
+        confirmPassword: "123456789",
+        companyName: "My Company",
+        companySite: "www.newcompany.com",
+        phoneNumber: "07738982323"
+    },
+    likes:[
+        {
+            userName: "user",
+            ideaId: "FB0WPpX25ES08N0hAL7NRbuCfyq1"
+        },
+        {
+            userName: "user",
+            ideaId: "FB0W25ESDFSFAERERAL7NRbuCfyq1"
+        }
+
+    ],
+    comments:[
+        {
+            userName: "user",
+            ideaId: "FB0W25ESDFSFAERERAL7NRbuCfyq1"
+        },
+        {
+            userName: "user",
+            ideaId: "FB0W25ESDFSFAERERAL7NRbuCfyq1"
+        }
+
+    ]
+
+}
