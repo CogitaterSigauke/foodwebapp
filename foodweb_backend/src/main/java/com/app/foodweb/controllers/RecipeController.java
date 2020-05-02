5f4f5e8dd2e33f5170b98bb1d3633f269646828f
MerryH23 <32797978+Heavenly23@users.noreply.github.com>
2020-05-02 10:41:21 +0900

    UserControllers and RecipeControllers and BlockUser repository aare added

--- foodweb_backend/src/main/java/com/app/foodweb/controllers/RecipeController.java
+++ foodweb_backend/src/main/java/com/app/foodweb/controllers/RecipeController.java
@@ -1,7 +1,9 @@
 package com.app.foodweb.controllers;
 
 import com.app.foodweb.models.Recipe;
+import com.app.foodweb.models.User;
 import com.app.foodweb.repositories.RecipeRepository;
+import com.app.foodweb.repositories.UserRepository;
 
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
@@ -20,8 +22,65 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class RecipeController {
-	
-	
- 
-                
+
+    @Autowired
+
+		RecipeRepository recipeRepository;
+
+		UserRepository userRepository;
+
+		@RequestMapping(method=RequestMethod.POST, value="app/add/recipe")
+    public Recipe save(@PathVariable String id,@RequestBody Recipe recipe) {
+        if(userRepository.findById(id).get().getActive().equals("true")){
+				   recipeRepository.save(recipe);
+           return recipe;
+				 }
+				 return null;
+    }
+
+		@RequestMapping(method=RequestMethod.PUT, value="app/edit_recipe/{id}")
+ 	 public Recipe updateRecipe(@PathVariable String id, @RequestBody Recipe recipe){
+          Recipe r = recipeRepository.findById(id).get();
+
+ 					if(recipe.getMealName() != null){
+             r.setMealName(recipe.getMealName());
+ 		      }
+ 					if(recipe.getMealType() != null){
+             r.setMealType(recipe.getMealType());
+ 		      }
+ 					if(recipe.getDietAndHealth() != null){
+             r.setDietAndHealth(recipe.getDietAndHealth());
+ 		      }
+ 					if(recipe.getWorldCuisine() != null){
+             r.setWorldCuisine(recipe.getWorldCuisine());
+ 		      }
+ 					if(recipe.getDescription() != null){
+             r.setDescription(recipe.getDescription());
+ 		      }
+ 					if(recipe.getImageBase64() != null){
+             r.setImageBase64(recipe.getImageBase64());
+ 		      }
+ 					if(recipe.getImage() != null){
+             r.setImage(recipe.getImage());
+ 		      }
+
+ 					recipeRepository.save(r);
+          return r;
+
+ 	 }
+
+		@RequestMapping(method=RequestMethod.DELETE, value="app/{user_id}/delete_recipe/{recipe_id}")
+ 	  public String deleteRecipe(@PathVariable String user_id,@PathVariable String recipe_id){
+				 Recipe recipe = recipeRepository.findById(recipe_id).get();
+				 // recipe can only be deleted by the owner of the recipe.
+		 		 if(recipe.getUserId().equals(user_id)){
+		         recipeRepository.delete(recipe);
+		         return "";
+         }
+				 return null;
+ 	 }
+
+
+
+
 }
