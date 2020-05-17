67fb2103e254496c073d8ec4365b5a57d4d84d18
SigaukeCogitater.com <cogi@gmail.com>
2020-05-18 07:12:37 +0900

    Implemented massaging backend

--- foodweb_backend/src/main/java/com/app/foodweb/FoodwebApplication.java
+++ foodweb_backend/src/main/java/com/app/foodweb/FoodwebApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import  java.security.Security; //for algolia search
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 @SpringBootApplication
 public class FoodwebApplication {
 
@@ -11,5 +16,16 @@ public class FoodwebApplication {
 		Security.setProperty("networkaddress.cache.ttl", "60");
 		SpringApplication.run(FoodwebApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/app/signup").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
 
 }
--- foodweb_backend/src/main/java/com/app/foodweb/repositories/MessageRepository.java
+++ foodweb_backend/src/main/java/com/app/foodweb/repositories/MessageRepository.java
@@ -4,9 +4,20 @@ import java.util.List;
 
 import com.app.foodweb.models.Message;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.domain.Sort;
+
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 
 public interface MessageRepository extends CrudRepository<Message, String> {
     @Override
     void delete(Message deleted);
-
-    }
+    
+    @Query("{$or : [{ $and: [ { 'senderUserId': { $eq: '?0' } }, { 'receiverUserId': {$eq: '?1'} } ] }, { $and: [ { 'senderUserId': { $eq: '?1' } }, { 'receiverUserId': {$eq: '?0'} } ] } ]}")
+    List<Message> findMessages(String senderUserId, String receiverUserId);
+    // , Sort sort);
+    
+}
