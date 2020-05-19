95eb725c22db751131b46a9d84c281ae84c95d74
SigaukeCogitater.com <cogi@gmail.com>
2020-05-19 11:03:11 +0900

    Changed get messages to return Json string

--- foodweb_backend/src/main/java/com/app/foodweb/controllers/MessageController.java
+++ foodweb_backend/src/main/java/com/app/foodweb/controllers/MessageController.java
@@ -23,6 +23,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 
+//JSON ARRAY
+
+// import org.json.JSONArray;  
+import com.google.gson.Gson;
+// import com.google.code.gson;
 //JAVA
 
 import java.util.List;
@@ -50,11 +55,15 @@ public class MessageController {
 
     //primary user is the user who is requesting for the messages  		
     @RequestMapping(method=RequestMethod.GET, value="app/message/{primaryUserId}/{secondaryUserId}")
-    public List<Message> getRecentMessages(@PathVariable String primaryUserId, @PathVariable String secondaryUserId) {
+    public String getRecentMessages(@PathVariable String primaryUserId, @PathVariable String secondaryUserId) {
        
 	List<Message> messages = messageRepository.findMessages(primaryUserId, secondaryUserId);
     messages.sort((Message m1, Message m2)->LocalDateTime.parse(m2.getCreatedAt()).compareTo(LocalDateTime.parse(m1.getCreatedAt())));
-        return messages;
+        
+        Gson gson = new Gson();
+        String messagesJson = gson.toJson(messages);
+        
+        return messagesJson;
     }
 
     @RequestMapping(method=RequestMethod.DELETE, value="app/message/{messageId}")
