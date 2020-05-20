dbd4c4bd4d0ff3c419e3818f47fd625fadf7e408
SigaukeCogitater.com <cogi@gmail.com>
2020-05-20 19:09:57 +0900

    Added Logic to Chat Box

--- foodweb_frontend/src/ChatBox.js
+++ foodweb_frontend/src/ChatBox.js
@@ -1,173 +1,194 @@
 import React, { Component } from 'react';
-import './ChatBox.css';
 
+import { Link } from 'react-router-dom';
+import Chat from './Chat';
 
-
-class Chat extends Component{
-    render(){
+function ChatBox() {
+  
         return(
-            
-<div class="container" id= "chatContainer">
-	<div class="row no-gutters">
-	  <div class="col-md-4 border-right">
-		<div class="settings-tray">
-		  <img class="profile-image" src={require("./profiles/ben.png")} alt="Profile img"/>
-		  <span class="settings-tray--right">
-      {/* <!-- <i class="material-icons">cached</i> --> */}
-      <i class="fas fa-envelope-open-text"></i>
-      {/* <!-- <i class="material-icons">message</i> --> */}
-      <i class="fas fa-bars"></i>
-			{/* <!-- <i class="material-icons">menu</i> --> */}
-		  </span>
-		</div>
-		<div class="search-box">
-		  <div class="input-wrapper">
-            <i class="fas fa-search"></i>
-			{/* <i class="material-icons">search</i> */}
-			<input placeholder="Search here" type="text"/>
-		  </div>
-		</div>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src={require("./profiles/daryl.png")} alt=""/>
-		  <div class="text">
-			<h6>Robo Cop</h6>
-			<p class="text-muted">Hey, you're arrested!</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr/>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src={require("./profiles/douglas.png")}  alt=""/>
-		  <div class="text">
-			<h6>Optimus</h6>
-			<p class="text-muted">Wanna grab a beer?</p>
-		  </div>
-		  <span class="time text-muted small">00:32</span>
-		</div>
-		<hr/>
-		<div class="friend-drawer friend-drawer--onhover ">
-		  <img class="profile-image" src={require("./profiles/stan.jpeg")} alt=""/>
-		  <div class="text">
-			<h6>Skynet</h6>
-			<p class="text-muted">Seen that canned piece of s?</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr/>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src={require("./profiles/sarah.jpeg")} alt=""/>
-		  <div class="text">
-			<h6>Termy</h6>
-			<p class="text-muted">Im studying spanish...</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr/>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src={require("./profiles/jacob.png")}  alt=""/>
-		  <div class="text">
-			<h6>Richard</h6>
-			<p class="text-muted">I'm not sure...</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr/>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src={require("./profiles/john.jpeg")}  alt=""/>
-		  <div class="text">
-			<h6>Alex</h6>
-			<p class="text-muted">Hi, wanna see something?</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-	  </div>
-	  <div class="col-md-8">
-		<div class="settings-tray">
-			<div class="friend-drawer no-gutters friend-drawer--grey">
-			<img class="profile-image" src={require("./profiles/daryl.png")}  alt=""/>
-			<div class="text">
-			  <h6>Robo Cop</h6>
-			  <p class="text-muted">Layin' down the law since like before Christ...</p>
-			</div>
-			<span class="settings-tray--right">
-			  {/* <!-- <i class="material-icons">cached</i> */}
-			  {/* <i class="material-icons">message</i> */}
-        {/* <i class="material-icons">menu</i> --> */}
-        <i class="fas fa-envelope-open-text"></i>
-        {/* <!-- <i class="material-icons">message</i> --> */}
-        <i class="fas fa-bars"></i>
-			</span>
-		  </div>
-		</div>
-		<div class="chat-panel">
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3 offset-md-9">
-			  <div class="chat-bubble chat-bubble--right">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3 offset-md-9">
-			  <div class="chat-bubble chat-bubble--right">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3 offset-md-9">
-			  <div class="chat-bubble chat-bubble--right">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row">
-			<div class="col-12">
-			  <div class="chat-box-tray">
-          <i class="far fa-smile"></i>
-				{/* <!-- <i class="material-icons">sentiment_very_satisfied</i> --> */}
-        <input type="text" placeholder="Type your message here..."/>
-     
-        <i class="fas fa-microphone"></i>
-        <i class="far fa-paper-plane"></i>
-				{/* <!-- <i class="material-icons">mic</i> */}
-				{/* <i class="material-icons">send</i> --> */}
-			  </div>
-			</div>
-		  </div>
-		</div>
-	  </div>
-	</div>
-  </div>
+            <div id="content-wrapper" className="d-flex flex-column">
+
+            <div id="content">
+           
+                
+        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+            <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+            <i className="fa fa-bars"></i>
+            </button>
+            {/* Search Bar */}
+
+            {/* <header>
+                    <div className="input-group">
+                    < SearchBox translations={{placeholder: 'Search for Recipes'}}  autoFocus defaultRefinement="Salad"/>
+                    </div>
+                </header>  */}
+
+            {/* nav bar  */}
+
+            <ul className="navbar-nav ml-auto">
+
+            <li className="nav-item dropdown no-arrow d-sm-none">
+                <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i className="fas fa-search fa-fw"></i>
+                </a>
+                <div className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <form className="form-inline mr-auto w-100 navbar-search">
+                    <div className="input-group">
+                    <input type="text" className="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"/>
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" type="button">
+                        <i className="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                    </div>
+                </form>
+                </div>
+            </li>
+
+            <li className="nav-item dropdown no-arrow mx-1">
+                <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i className="fas fa-bell fa-fw"></i>
+                <span className="badge badge-danger badge-counter">3+</span>
+                </a>
+                <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                <h6 className="dropdown-header">
+                    Alerts Center
+                </h6>
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                    <div className="mr-3">
+                    <div className="icon-circle bg-primary">
+                        <i className="fas fa-file-alt text-white"></i>
+                    </div>
+                    </div>
+                    <div>
+                    <div className="small text-gray-500">December 12, 2019</div>
+                    <span className="font-weight-bold">A new monthly report is ready to download!</span>
+                    </div>
+                </a>
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                    <div className="mr-3">
+                    <div className="icon-circle bg-success">
+                        <i className="fas fa-donate text-white"></i>
+                    </div>
+                    </div>
+                    <div>
+                    <div className="small text-gray-500">December 7, 2019</div>
+                    $290.29 has been deposited into your account!
+                    </div>
+                </a>
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                    <div className="mr-3">
+                    <div className="icon-circle bg-warning">
+                        <i className="fas fa-exclamation-triangle text-white"></i>
+                    </div>
+                    </div>
+                    <div>
+                    <div className="small text-gray-500">December 2, 2019</div>
+                    Spending Alert: We've noticed unusually high spending for your account.
+                    </div>
+                </a>
+                <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                </div>
+            </li>
+
+            <li className="nav-item dropdown no-arrow mx-1">
+            <Link to="/ChatBox" className="nav-link dropdown-toggle" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i className="fas fa-envelope fa-fw"></i>
+                <span className="badge badge-danger badge-counter">7</span>
+            </Link>
+                <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
+                <h6 className="dropdown-header">
+                    Message Center
+                </h6>
+                <Link to="/ChatBox" className="dropdown-item d-flex align-items-center" >
+                    <div className="dropdown-list-image mr-3">
+                    <img className="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt=""/>
+                    <div className="status-indicator bg-success"></div>
+                    </div>
+                    <div className="font-weight-bold">
+                    <div className="text-truncate">Hi there! I am wondering if you can help me with a problem I've been having.</div>
+                    <div className="small text-gray-500">Emily Fowler · 58m</div>
+                    </div>
+                </Link>
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                    <div className="dropdown-list-image mr-3">
+                    <img className="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt=""/>
+                    <div className="status-indicator"></div>
+                    </div>
+                    <div>
+                    <div className="text-truncate">I have the photos that you ordered last month, how would you like them sent to you?</div>
+                    <div className="small text-gray-500">Jae Chun · 1d</div>
+                    </div>
+                </a>
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                    <div className="dropdown-list-image mr-3">
+                    <img className="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt=""/>
+                    <div className="status-indicator bg-warning"></div>
+                    </div>
+                    <div>
+                    <div className="text-truncate">Last month's report looks great, I am very happy with the progress so far, keep up the good work!</div>
+                    <div className="small text-gray-500">Morgan Alvarez · 2d</div>
+                    </div>
+                </a>
+                <a className="dropdown-item d-flex align-items-center" href="#">
+                    <div className="dropdown-list-image mr-3">
+                    <img className="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt=""/>
+                    <div className="status-indicator bg-success"></div>
+                    </div>
+                    <div>
+                    <div className="text-truncate">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</div>
+                    <div className="small text-gray-500">Chicken the Dog · 2w</div>
+                    </div>
+                </a>
+                <a className="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                </div>
+            </li>
+
+            <div className="topbar-divider d-none d-sm-block"></div>
+
+            <li className="nav-item dropdown no-arrow">
+                <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span className="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
+                <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60"/>
+                </a>
+                <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a className="dropdown-item" href="#">
+                    <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Profile
+                </a>
+                <a className="dropdown-item" href="#">
+                    <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Settings
+                </a>
+                <a className="dropdown-item" href="#">
+                    <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Activity Log
+                </a>
+                <Link to = "/AddRecipe">
+                    <p className="dropdown-item">
+                    <i className="fas fa-glass-cheers fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Add Recipe
+                    </p>
+                </Link>
+                <div className="dropdown-divider"></div>
+                <Link to= "/">
+                    <p className="dropdown-item"  data-toggle="modal" data-target="#logoutModal">
+                    <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                    </p>
+                </Link>
+                </div>
+            </li>
+
+            </ul>
+
+            </nav>
+
+            <Chat/>
+            </div>
+            </div>
         );
-    }
+    
 }
-export default Chat;
+export default ChatBox;
