6c427d0cfcdfa0839be6e6d9374933f525df62fc
RediIssac <46311588+RediIssac@users.noreply.github.com>
2020-06-03 18:22:09 +0900

    Update AddRecipe.js

--- foodweb_frontend/src/AddRecipe.js
+++ foodweb_frontend/src/AddRecipe.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
